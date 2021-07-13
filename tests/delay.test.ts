e2091ed95c66c9195fbb79363927e32a098291ee
David Timms <davidtimms@btinternet.com>
2021-07-13 21:54:50 +0100

    Fix type error in delay tests

--- tests/delay.test.ts
+++ tests/delay.test.ts
@@ -7,8 +7,8 @@ describe("The delay method", () => {
       IO.wrap("result").delay(15, "milliseconds")
     );
 
-    expect(result.outcome).toBe(IOOutcome.Succeeded);
-    expect(result.value).toBe("result");
+    expect(result.outcome === IOOutcome.Succeeded).toBe(true);
+    expect((result as any).value).toBe("result");
     expect(timeoutTrace).toEqual([["setTimeout", 15, expect.anything()]]);
   });
 });
